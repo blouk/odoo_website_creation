8ee67512ea568758b70ffe59e3c1077a9f81a378
blouk <nicolas@blouk.com>
2015-12-07 19:54:00 +0100

    [ADD]

--- website_tutorials/static/src/js/website.js
+++ website_tutorials/static/src/js/website.js
@@ -2,39 +2,35 @@
     'use strict';
     var website = openerp.website;
 
-    if (!website.snippet) website.snippet = {};
-
-
     website.snippet.animationRegistry.website_snippet = website.snippet.Animation.extend({
         selector: ".website_snippet",
         i: 0,
         animate_time: 0,
-        animation_s: true,
-        colors: ['#FFB30C', '#58EC00', '#0087EC', '#EEEEEE', '#FF5A00'],
-        start: function() {
 
-            this.loop_animation();
+        colors: ['#FFB30C', '#58EC00', '#0087EC', '#EEEEEE', '#FF5A00'],
+        start: function(editable_mode) {
+            if (!editable_mode) {
+                this.loop_animation();
+            }
         },
         loop_animation: function() {
             var self = this;
             window.clearInterval(self.animate_time);
-            if (self.animation_s) {
-                self.animate_time = window.setInterval(
-                    function() {
-                        self.$target.find('.container').animate({
-                            backgroundColor: self.colors[(self.i++) % self.colors.length]
-                        });
-                    }, 500
-                );
-            }
+
+            self.animate_time = window.setInterval(
+                function() {
+                    self.$target.find('.container').animate({
+                        backgroundColor: self.colors[(self.i++) % self.colors.length]
+                    });
+                }, 500
+            );
+
         },
         stop: function() {
             var self = this;
-            self.animation_s = false;
             self.$target.find('.container').clearQueue();
             self.$target.find('.container').stop();
             window.clearInterval(self.animate_time);
         }
     });
-
 })();
