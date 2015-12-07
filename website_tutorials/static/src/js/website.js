2c54b59bdb4b39fd7406a39f773f7d7843f2aa86
blouk <nicolas@blouk.com>
2015-12-07 20:02:19 +0100

    [ADD]

--- website_tutorials/static/src/js/website.js
+++ website_tutorials/static/src/js/website.js
@@ -3,7 +3,7 @@
     var website = openerp.website;
     var qweb = openerp.qweb;
 
-    if (!website.snippet) website.snippet = {};
+
 
     website.add_template_file('/website_tutorials/static/src/xml/template.xml');
 
@@ -11,34 +11,33 @@
         selector: ".website_snippet",
         i: 0,
         animate_time: 0,
-        animation_s: true,
-        colors: ['#FFB30C', '#58EC00', '#0087EC', '#EEEEEE', '#FF5A00'],
-        start: function() {
 
-            this.loop_animation();
+        colors: ['#FFB30C', '#58EC00', '#0087EC', '#EEEEEE', '#FF5A00'],
+        start: function(editable_mode) {
+            if (!editable_mode) {
+                this.loop_animation();
+            }
         },
         loop_animation: function() {
             var self = this;
             window.clearInterval(self.animate_time);
-            if (self.animation_s) {
-                self.animate_time = window.setInterval(
-                    function() {
-                        self.$target.find('.container').animate({
-                            backgroundColor: self.colors[(self.i++) % self.colors.length]
-                        });
-                    }, 500
-                );
-            }
+
+            self.animate_time = window.setInterval(
+                function() {
+                    self.$target.find('.container').animate({
+                        backgroundColor: self.colors[(self.i++) % self.colors.length]
+                    });
+                }, 500
+            );
+
         },
         stop: function() {
             var self = this;
-            self.animation_s = false;
             self.$target.find('.container').clearQueue();
             self.$target.find('.container').stop();
             window.clearInterval(self.animate_time);
         }
     });
-
 
     website.snippet.animationRegistry.last_products = website.snippet.Animation.extend({
         selector: ".website_products",
@@ -62,9 +61,9 @@
                 }));
             });
         },
-        stop: function(){
+        stop: function() {
             var self = this;
-            this.$target.fadeTo( 'slow', 0.33 );
+            this.$target.fadeTo('slow', 0.33);
         }
     });
 
