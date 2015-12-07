46a35970417bbec2fbc6e099cb64f6120d0174cd
blouk <nicolas@blouk.com>
2015-12-07 14:46:54 +0100

    [ADD]

--- website_tutorials/static/src/js/website.js
+++ website_tutorials/static/src/js/website.js
@@ -1,9 +1,11 @@
 (function() {
     'use strict';
     var website = openerp.website;
+    var qweb = openerp.qweb;
 
     if (!website.snippet) website.snippet = {};
 
+    website.add_template_file('/website_tutorials/static/src/xml/template.xml');
 
     website.snippet.animationRegistry.website_snippet = website.snippet.Animation.extend({
         selector: ".website_snippet",
@@ -34,6 +36,35 @@
             self.$target.find('.container').clearQueue();
             self.$target.find('.container').stop();
             window.clearInterval(self.animate_time);
+        }
+    });
+
+
+    website.snippet.animationRegistry.last_products = website.snippet.Animation.extend({
+        selector: ".website_products",
+        start: function(editable_mode) {
+            var self = this;
+
+            if (editable_mode) {
+                this.$target.find('.container').attr('contentEditable', 'false');
+                return;
+            }
+
+            $.ajax({
+                url: '/website/action/get_products',
+                method: 'GET',
+                self: self
+
+            }).done(function(data) {
+                var self = this.self;
+                self.$target.find('.product_container').empty().append(qweb.render('website_tutorials.last_products', {
+                    'products': data
+                }));
+            });
+        },
+        stop: function(){
+            var self = this;
+            this.$target.fadeTo( 'slow', 0.33 );
         }
     });
 
